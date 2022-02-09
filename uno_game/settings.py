d7f1be9d49807003124f7f6dd099a2c3f22d1da7
regalk13 <jsalazarcaviedes30@gmail.com>
2022-02-09 18:23:17 -0500

    :wrench: Work static files, :sparkles: styling

--- uno_game/settings.py
+++ uno_game/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,14 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_ROOT = ''
+STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
 
-STATICFILES_DIR = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = (os.path.join('static'),)
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
