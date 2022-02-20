aa7d739d0dbcaabac52a8cf18cfd28223cfe50df
regalk13 <jsalazarcaviedes30@gmail.com>
2022-02-20 10:55:03 -0500

    :wrench: Create/Join on public/private rooms

--- game/views.py
+++ game/views.py
@@ -13,18 +13,40 @@ MINIMUM_ONLINE_PLAYER_REQUIRED = 3
 @login_required(login_url='login')
 def game_now(request):
     player = request.user
+    game_select = 1
     if not player.is_authenticated:
         message = f"You need to login first!"
         return redirect('login')
-    
     if request.method == 'POST':
-        print(request.POST.get('name').lower())
-        print(request.POST.get('players'))
         if request.POST.getlist('public'):
-            print("true")
+            game_select = 0
         else:
-            print("false")
+            game_select = 1
+        
+        unique_id = id_generator(10)
+        GameServer.create_new_game(unique_id=unique_id, player=player, game_type=game_select)
+        return HttpResponseRedirect(
+            reverse('enter_game', kwargs={'game_type': game_select, 'unique_id': unique_id})
+        )
 
+    if request.method == 'GET':
+        unique_id = request.GET.get('unique_id')
+        if unique_id:
+            for private_game in GameServer.AVAILABLE_PRIVATE_GAMES:
+                if unique_id == private_game.unique_id:
+                    if private_game.get_count_of_players() < MAX_JOINED_PLAYER_COUNT:
+                        if not private_game.is_game_running:
+                            private_game.join_player(player)
+                            return HttpResponseRedirect(
+                                reverse('enter_game', kwargs={'game_type': GameServer.PRIVATE, 'unique_id': unique_id}))
+            
+            for public_game in GameServer.AVAILABLE_PUBLIC_GAMES:
+                if unique_id == public_game.unique_id:
+                    if public_game.get_count_of_players() < MAX_JOINED_PLAYER_COUNT:
+                        if not public_game.is_game_running:
+                            public_game.join_player(player)
+                            return HttpResponseRedirect(
+                                reverse('enter_game', kwargs={'game_type': GameServer.PUBLIC, 'unique_id': unique_id}))
     context = {
         "public_rooms": GameServer.AVAILABLE_PUBLIC_GAMES
     }
@@ -36,10 +58,6 @@ def enter_public_play(request, unique_id):
         if GameServer.AVAILABLE_PUBLIC_GAMES:
             for public_game in GameServer.AVAILABLE_PUBLIC_GAMES:
                 if unique_id == public_game.unique_id:
-                    for player_public in public_game.players:
-                        if player_public.username == player.username:
-                            return redirect('game-now')
-                    print(public_game.get_count_of_players())
                     if public_game.get_count_of_players() < MAX_JOINED_PLAYER_COUNT:
                         if not public_game.is_game_running:
                                 public_game.join_player(player)
